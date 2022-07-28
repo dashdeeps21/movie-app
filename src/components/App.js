ca95786f8aee80775042fa6930a27d4c3d917547
dashdeeps21 <deepakkadela21@gmail.com>
2022-07-28 16:52:34 +0530

    Subscribe to the store

--- src/components/App.js
+++ src/components/App.js
@@ -1,25 +1,49 @@
+import React from "react";
 import { data } from '../data';
-import Navbar from './Navbar';
-import MovieCard from './MovieCard';
+import Navbar from "./Navbar";
+import MovieCard from "./MovieCard";
 
-function App() {
-  return (
-    <div className="App">
-      <Navbar />
-      <div className="main">
+
+class App extends React.Component {
+
+  componentDidMount () {
+    const { store } = this.props;
+    store.subscribe(() => {
+        console.log('UPDATED');
+        this.forceUpdate();
+    });
+    //make api call
+    //dispatch action
+
+    store.dispatch({
+      type: 'ADD_MOVIES',
+      movies: data
+    });
+    
+   console.log('STATE', this.props.store.getState());
+  }
+
+  render() {
+    const movies = this.props.store.getState();
+    console.log('RENDER');
+    return (
+      <div className="App">
+        <Navbar />
+        <div className="main">
           <div className="tabs">
             <div className="tab">Movies</div>
             <div className="tab">Favourites</div>
           </div>
 
           <div className="list">
-            {data.map(movie => (
-              <MovieCard movie={movie} />
+            {movies.map((movie, index) => (
+              <MovieCard movie={movie} key={`movies-${index}`} />
             ))}
           </div>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
